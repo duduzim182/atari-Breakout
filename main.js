d6ffd02b0880f29fa13fda4e62651c8f9f79dbcb
Eduardo Salermo <duduscotta@gmail.com>
2025-10-28 16:53:52 -0300

    3 commit

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function setup() {
         x:width/2,
         y:height/2,
         r:10,
-        vx: random(-3,3),
-        vy: random(-3,3)
+        vx: 0,
+        vy: 0
     };
     paddle ={
         x: width /2,
@@ -29,4 +29,19 @@ function draw() {
     rectMode(CENTER)
     rect(paddle.x, paddle.y, paddle.w, paddle.h)
     paddle.x=constrain(mouseX, paddle.w/2, width-paddle.w/2)
+    if (ball.x - ball.r<0||ball.x+ ball.r>width) ball.vx*=-1;
+    if (ball.y - ball.r<0) ball.vy*=-1;
+    if (ball.y + ball.r > paddle.y - paddle.h/2 &&
+        ball.y + ball.r > paddle.y + paddle.h/2 &&
+        ball.x > paddle.x - paddle.w/2 &&
+        ball.x < paddle.x + paddle.w/2
+    ){
+        ball.vy*=-1
+       let diff =ball.x - paddle.x
+       ball.vx=diff*0.1
+    }
+}
+function mousePressed(){
+    ball.vx = random(4, -4)
+    ball.vy = -5
 }
